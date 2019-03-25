7aedd95f30f5694ccf084930e02c56bf6ae9607d
Chad4545 <genom1324@gmail.com>
2019-03-25 20:44:41 +0900

    sq/abs/huber/

---
+++ homework/4/homework1.R
@@ -0,0 +1,58 @@
+getwd()
+setwd("/Users/sungjinpark/Desktop/OneDrive - konkuk.ac.kr/datamining/homework/4")
+# data
+xy.df = read.csv(file='old.sam.for.reg.fit.csv')
+nxy.df = read.csv(file='old.sam.for.reg.pred.csv')
+# result matrix
+rst.mat = matrix(NA,4,3)
+colnames(rst.mat)=c("sq","abs","hub")
+rownames(rst.mat) =c("R^2","Ra^2",'AIC',"pred_error")
+#rst.mat
+
+
+'''
+1. data의 변수를 모두 다 사용
+2. 기본 linear 모델, abs loss 모델, huber loss 모델 모두 사용
+3. R^2, adj R^2, AIC, pred_error 의 평가지표 모두 사용 
+'''
+
+colnames(xy.df)
+#1 linear - square loss
+lm.fit = lm(formula=sensitivity~.,data=xy.df)
+#names(lm.fit)
+#lm.summ = summary(lm.fit)
+#names(lm.summ)
+rst.mat[,1]=c(summary(lm.fit)$r.squared,
+              summary(lm.fit)$adj.r.squared,
+              AIC(lm.fit),
+              mean((nxy.df[,1]-predict(lm.fit,newdata=nxy.df))^2)) # y_hat
+
+
+# abs loss
+library(quantreg)
+rq.fit = rq(formula=sensitivity~.,data=xy.df)
+#names(rq.fit)
+#rq.summ = summary(rq.fit)
+#names(rq.summ)
+## R^2, adjR^2가 없다!  SST의 분해는 제곱로스일 때만 가능하다 
+rst.mat[,2]=c(NA,NA,AIC(rq.fit),
+              mean((nxy.df[,1]-predict(rq.fit,newdata=nxy.df))^2))
+rst.mat
+
+
+
+# 3. huber loss
+library(robustreg)
+require(MASS)
+#names(rb.fit) # [1] "coefficients" "weights"      "mse" 
+#rb.summ=summary(rb.fit)
+#names(rb.summ)
+# 후버에 대해서 숙제 
+## R^2, adjR^2가 없다!  SST의 분해는 제곱로스일 때만 가능하다 
+
+rb.fit <- rlm(sensitivity~.,data=xy.df, k2 = 1.345) 
+rst.mat[,3]=c(NA,NA,AIC(rb.fit),
+              mean((nxy.df[,1]-predict(rb.fit,newdata=nxy.df))^2))
+rst.mat
+
+
